53fe29c56b1a181d6f18d8d9768a536e4a5f0b92
koonlx <ljh1223582317@gmail.com>
2024-04-15 01:31:48 +0900

    fix image url error

--- src/main/java/com/caterpillars/StayConnect/repository/UserRepository.java
+++ src/main/java/com/caterpillars/StayConnect/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.caterpillars.StayConnect.repository;
 
 
-import com.caterpillars.StayConnect.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.caterpillars.StayConnect.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
 }
