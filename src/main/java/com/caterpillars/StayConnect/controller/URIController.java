a1707043a36945032404341f469fcb24a214b6b7
koonlx <ljh1223582317@gmail.com>
2024-04-22 19:30:30 +0900

    1차 수정

---
+++ src/main/java/com/caterpillars/StayConnect/controller/URIController.java
@@ -0,0 +1,29 @@
+package com.caterpillars.StayConnect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class URIController {
+
+  @GetMapping("/signup")
+  public String showSignUpForm() {
+    return "auth/signup";
+  }
+
+  @GetMapping("/signin")
+  public String signin() {
+    return "auth/signin";
+  }
+
+  @GetMapping("/mypage")
+  public String myPage() {
+    return "auth/mypage";
+  }
+
+  @GetMapping("/admin/user/list")
+  public String getUserList() {
+    return "admin_user";
+  }
+
+}
