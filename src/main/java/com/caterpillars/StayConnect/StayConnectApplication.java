77f68a0118a434fc15d8f2be3f14f20d3cae7819
koonlx <ljh1223582317@gmail.com>
2024-05-22 11:25:53 +0900

    change settings to jar packaging

--- src/main/java/com/caterpillars/StayConnect/StayConnectApplication.java
+++ src/main/java/com/caterpillars/StayConnect/StayConnectApplication.java
@@ -2,10 +2,8 @@ package com.caterpillars.StayConnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-// @SpringBootApplication
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class StayConnectApplication {
 
 	public static void main(String[] args) {
