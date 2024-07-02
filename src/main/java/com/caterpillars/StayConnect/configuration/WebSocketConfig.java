495cfdf3cc0c5bcb13d55728b525ba2096a49302
koonlx <ljh1223582317@gmail.com>
2024-07-03 06:44:59 +0900

    solve cors error attempt 4

--- src/main/java/com/caterpillars/StayConnect/configuration/WebSocketConfig.java
+++ src/main/java/com/caterpillars/StayConnect/configuration/WebSocketConfig.java
@@ -25,6 +25,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/chat/**").setAllowedOriginPatterns("*").withSockJS();
     }
 
 
