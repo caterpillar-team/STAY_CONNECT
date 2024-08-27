bed813137cb1a6a6d7039600f14dd9b99ce8f30c
koonlx <ljh1223582317@gmail.com>
2024-08-27 11:25:13 +0900

    modified: Initializers

--- src/main/java/com/caterpillars/StayConnect/model/repository/RoleRepository.java
+++ src/main/java/com/caterpillars/StayConnect/model/repository/RoleRepository.java
@@ -1,5 +1,7 @@
 package com.caterpillars.StayConnect.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.caterpillars.StayConnect.model.entities.Role;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-  Role findByName(String roleName);
+  Optional<Role> findByName(String roleName);
 }
